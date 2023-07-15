9ac73c98eeafd60108ba73054696a66c1f5fdd92
gon2santos <necchigonzalo@gmail.com>
2023-07-15 09:54:23 -0300

    impl get flights route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const body_parser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+var carouselData = require('./data');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const dotenv = require('dotenv').config();
+/* import { carruselesData } from './data'; */
 
 const app = express();
 const PORT = 3000;
@@ -22,14 +26,32 @@ app.use((_req, res, next) => {
 app.use((0, cors)());
 
 app.listen(PORT, (error) => {
-    if (!error)
+    if (!error) {
         console.log("Server is Successfully Running, listening on port " + PORT)
+    }
     else
         console.log("Error occurred, server can't start", error);
 }
 );
 
 app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.status(200).send("<p>Server is running OK</p>");
+    res.set('Content-Type', 'application/json');
+    res.status(200).json({ response: "OK!" }); /* send("<p>Server is running OK</p>"); */
 });
+
+app.get('/carruseles', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    var resData = [];
+    carouselData.default.forEach(function (obj, i) {
+        resData.push({ airportCode: obj.airportCode, flightCode: obj.flightCode, carousel: obj.carousel, id: i });
+    });
+    res.status(200).json(resData); /* send("<p>Server is running OK</p>"); */
+});
+
+app.get('/flights', (req, res) => {
+    console.log("Request AEP flights data");
+    fetch(`https://airlabs.co/api/v9/schedules?dep_iata=AEP&api_key=${process.env.AIRLABS_KEY}`)
+        .then(response => response.json())
+        .then(response => res.json(response))
+        .catch(e => res.status(500).send(`Error fetching flights: ${e.message}`))
+})
